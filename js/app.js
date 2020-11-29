0d314e473aa1ac00d88ce1bd84e1ca1fde95a8c8
Howard <howardjer@gmail.com>
2020-11-28 23:48:18 -0700

    changes page on each click

--- js/app.js
+++ js/app.js
@@ -3,39 +3,48 @@ let target = document.querySelector('.advice');
 
 // select the button and attach an event listener to it
 let button = document.querySelector('.get-advice');
-button.addEventListener('click', function() {
+button.addEventListener('click', function () {
+    var buton = document.getElementById("button");
+    var allchar = "0123456789ABCDEF";
 
-    document.body.style.backgroundColor = "blue";
-    document.body.style.backgroundColor = "white";
-    
+    button.addEventListener("click", pageChange);
+
+    function pageChange() {
+        var randcol = "";
+        for (var i = 0; i < 6; i++) {
+            randcol += allchar[Math.floor(Math.random() * 16)];
+        }
+        document.body.style.backgroundColor = "#" + randcol;
+    }
+
     // create a new XMLHttpRequest object
     let ajax = new XMLHttpRequest();
 
     // define what happens when the ajax request is performed
-    ajax.onreadystatechange = function() {
-        
+    ajax.onreadystatechange = function () {
+
         // if the request is successful
-        if(this.readyState == 4 && this.status == 200) {
-            
+        if (this.readyState == 4 && this.status == 200) {
+
             // Parse the JSON string response in to a usable JS object
             let advisor = JSON.parse(this.responseText);
             // console.log(JSON.parse(this.responseText))
-                     
+
             // Set the <p>'s innerHTML
             target.innerHTML = advisor.slip.advice;
         }
-        else if(this.readyState != 4) {
+        else if (this.readyState != 4) {
             document.getElementById("adviceGiven").innerHTML = "LOADING";
         }
-            
-        
+
+
     }
 
     // define where and how to make your ajax request
-     
+
     ajax.open("GET", "	https://api.adviceslip.com/advice", true);   // args: request type, api url, asynchronous?
     // ajax.open("GET", "https://api.kanye.rest", true);   // args: request type, api url, asynchronous?
-    
+
     // trigger the ajax request (changes readyState)
     ajax.send();
 });
