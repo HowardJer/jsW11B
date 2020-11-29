3de6f12573cae36c3c035da5201ae541d22df531
Howard <howardjer@gmail.com>
2020-11-28 22:25:51 -0700

    used new api; needed cors, resolved undefine line 20; doesn't change on click

--- js/app.js
+++ js/app.js
@@ -1,8 +1,8 @@
 // target to <p> tag to fill in
-let target = document.querySelector('.quote');
+let target = document.querySelector('.titles');
 
 // select the button and attach an event listener to it
-let button = document.querySelector('.get-quote');
+let button = document.querySelector('.get-titles');
 button.addEventListener('click', function() {
     
     // create a new XMLHttpRequest object
@@ -15,18 +15,20 @@ button.addEventListener('click', function() {
         if(this.readyState == 4 && this.status == 200) {
             
             // Parse the JSON string response in to a usable JS object
-            let text = JSON.parse(this.responseText);
-            console.log(this.responseText)
-            
+            let recipeTitles = JSON.parse(this.responseText);
+            // console.log(JSON.parse(this.responseText))
+            let eachTitle = recipeTitles.results[length].title;
+            console.log(eachTitle)
             // Set the <p>'s innerHTML
-            target.innerHTML = text.quote;
+            target.innerHTML = eachTitle;
             
         } 
     }
 
     // define where and how to make your ajax request
      
-    ajax.open("GET", "https://api.kanye.rest", true);   // args: request type, api url, asynchronous?
+    ajax.open("GET", "https://cors-anywhere.herokuapp.com/http://www.recipepuppy.com/api/", true);   // args: request type, api url, asynchronous?
+    // ajax.open("GET", "https://api.kanye.rest", true);   // args: request type, api url, asynchronous?
     
     // trigger the ajax request (changes readyState)
     ajax.send();
